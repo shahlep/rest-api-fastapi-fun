87bd3faa438eee61fb0b3229f568d2c2620c5f82
Shah rahman <toshah.rahman@gmail.com>
2022-05-10 20:40:15 +0300

    fastapi fun 1

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return {"message": "Welcome to my library"}
+
+
 # /list all books
+@app.get("/list-all-books")
+async def list_all_books():
+    return {"message": "List all books"}
 # /book-by-index/
 # /get-random-book
 # /add-book
