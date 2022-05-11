811d304bcfb0866218bcb7017877075e767e8d11
Shah rahman <toshah.rahman@gmail.com>
2022-05-11 18:53:10 +0300

    fastapi fun 5

--- todo.py
+++ todo.py
@@ -2,15 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+todos = [
+    {
+        'id': '1',
+        'activity': 'getting fresh'
+    },
+    {
+        'id': '2',
+        'activity': 'make breakfast'
+    }
+]
+
 
 # minimal app - as suggested in https://fastapi.tiangolo.com/tutorial/first-steps/
 @app.get("/")
 async def home():
     return {"message": "Welcome to my todo list"}
 
+
 # Get --> Read Todo
+@app.get('/todos', tags=['todos'])
+async def get_todos():
+    return {'todos': todos}
 
 # POST --> Create Todo
 # PUT --> Update Todo
 # DELETE --> Delete Todo
-
