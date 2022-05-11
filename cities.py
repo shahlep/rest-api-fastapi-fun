0d9e442596ab1d2be280bba0cc4c976a4e7c6714
Shah rahman <toshah.rahman@gmail.com>
2022-05-11 20:40:41 +0300

    fastapi fun 9

--- cities.py
+++ cities.py
@@ -16,3 +16,14 @@ class City(BaseModel):
 @app.get('/')
 def index():
     return {'key': 'value'}
+
+
+@app.get('/cities')
+def get_cities():
+    return db
+
+
+@app.post('/cities')
+def create_city(city: City):
+    db.append(city.dict())
+    return db[-1]
