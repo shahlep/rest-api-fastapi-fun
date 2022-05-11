a9f937697745ce47891c6825b0cfd0f51393de3b
Shah rahman <toshah.rahman@gmail.com>
2022-05-11 20:03:09 +0300

    fastapi fun 6

--- todo.py
+++ todo.py
@@ -1,3 +1,5 @@
+import http
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -21,10 +23,15 @@ async def home():
 
 
 # Get --> Read Todo
-@app.get('/todos', tags=['todos'])
+@app.get('/todo', tags=['todos'], status_code=http.HTTPStatus.OK)
 async def get_todos():
     return {'todos': todos}
 
+
 # POST --> Create Todo
+@app.post('/todo', tags=['todos'], status_code=http.HTTPStatus.CREATED)
+async def create_todos(todo: dict):
+    todos.append(todo)
+    return {'data': 'New Item has been added'}
 # PUT --> Update Todo
 # DELETE --> Delete Todo
