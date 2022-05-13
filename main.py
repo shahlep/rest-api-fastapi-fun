c2995700089bde8b306b0d5e69ebbcccfcf19a5c
Shah rahman <toshah.rahman@gmail.com>
2022-05-13 17:52:44 +0300

    fastapi fun 21

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
 # query params
 @app.get('/blog')
-async def home(limit: int = 10, published: bool = True):
+async def home(limit: int = 10, published: bool = True, sort: Optional[str] = None):
     if published:
         return {'message': f'{limit} published blog from Blog lists!'}
     else:
