0e72eed28e1040d0cfb02eb7e390f50beee55dcc
Shah rahman <toshah.rahman@gmail.com>
2022-05-13 17:14:50 +0300

    fastapi fun 16

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ async def home():
     return {"message": "Blog lists"}
 
 
+@app.get('/blog/unpublished')
+async def unpublished():
+    return {'message': 'unpublished blogs'}
+
+
 @app.get('/blog/{id}')
-async def show(id:int):
+async def show(id: int):
     return {'message': id}
