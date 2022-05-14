114d443ad4663af654e25d1aa40d141ef1703273
Shah rahman <toshah.rahman@gmail.com>
2022-05-14 12:17:58 +0300

    fastapi fun 35

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,5 @@
 import pydantic as _pd
+import datetime as _dt
 
 
 class _PostBase(_pd.BaseModel):
@@ -8,3 +9,13 @@ class _PostBase(_pd.BaseModel):
 
 class _PostCreate(_PostBase):
     pass
+
+
+class Post(_PostBase):
+    id: int
+    owner_id: int
+    date_created: _dt.datetime
+    date_last_updated: _dt.datetime
+
+    class config:
+        orm_mode = True
