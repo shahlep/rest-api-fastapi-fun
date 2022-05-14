43e6b32fcff386bbaf36f4fa79b58350195f2ae3
Shah rahman <toshah.rahman@gmail.com>
2022-05-14 14:33:23 +0300

    fastapi fun 45

--- src/main.py
+++ src/main.py
@@ -11,4 +11,6 @@ _services.create_database()
 # https://fastapi.tiangolo.com/tutorial/sql-databases/
 @app.post('/users/', response_model=_schemas.User)
 def create_user(user: _schemas._UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db())):
-    pass
+    db_user = _services.get_user_by_email(db=db, email=user.email)
+    if db_user:
+        raise _fastapi.HTTPException(status_code=400,detail='email already in use!')
