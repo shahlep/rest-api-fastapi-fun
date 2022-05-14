accc1c9d814d3570cea3a61bfa75fafa217ef656
Shah rahman <toshah.rahman@gmail.com>
2022-05-14 15:25:05 +0300

    fastapi fun 49

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import fastapi as _fastapi
+from typing import List
 import sqlalchemy.orm as _orm
 import src.services as _services
 import src.schemas as _schemas
@@ -13,7 +14,10 @@ _services.create_database()
 def create_user(user: _schemas._UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db())):
     db_user = _services.get_user_by_email(db=db, email=user.email)
     if db_user:
-        raise _fastapi.HTTPException(status_code=400,detail='email already in use!')
+        raise _fastapi.HTTPException(status_code=400, detail='email already in use!')
     return _services.create_user(db=db, user=user)
 
 
+@app.get('/users/', response_model=[_schemas.User])
+def get_user(limit: int = 10, db: _orm.Session = _fastapi.Depends(_services.get_db())):
+    pass
