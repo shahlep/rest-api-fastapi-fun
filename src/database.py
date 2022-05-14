ce9f48aa13866dd0f303d99ac4459526c29fd643
Shah rahman <toshah.rahman@gmail.com>
2022-05-14 10:31:00 +0300

    fastapi fun 29

--- src/database.py
+++ src/database.py
@@ -1,7 +1,16 @@
+import sqlalchemy
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
+# https://fastapi.tiangolo.com/tutorial/sql-databases/
+
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
 
+engine = sqlalchemy.create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+)
 
+Sessionlocal = _orm.sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base = _declarative.declarative_base()
