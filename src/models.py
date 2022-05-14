50b69041796ed0143dde5bc516c90d882e3e80e0
Shah rahman <toshah.rahman@gmail.com>
2022-05-14 10:54:10 +0300

    fastapi fun 31

--- src/models.py
+++ src/models.py
@@ -3,3 +3,12 @@ import sqlalchemy.orm as _orm
 
 import database as _database
 
+
+class User(_database.Base):
+    __tablename__ = 'users'
+    id = _sql.column(_sql.Integer, primary_key=True, index=True)
+    email = _sql.column(_sql.String, unique=True,index=True)
+    password = _sql.column(_sql.String)
+    is_active = _sql.column(_sql.Boolean, default=True)
+
+
